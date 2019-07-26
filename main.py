cf77fc7f0614784d7ae55a61e8413243a7277359
Mikey <richardsm3@my.lowercolumbia.edu>
2019-07-26 15:59:19 -0700

    Added login.html

---
+++ main.py
@@ -0,0 +1,17 @@
+import webapp2
+import jinja2
+import os
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class LoginPage(webapp2.RequestHandler):
+    def get(self):
+        login_template = the_jinja_env.get_template('login.html');
+        self.response.write(login_template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', LoginPage),
+], debug=True);
